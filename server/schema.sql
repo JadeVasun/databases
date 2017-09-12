6cce5581cd679fe6cd31b80f6321880d59023844
Hack Reactor Student <student@hackreactor.com>
2017-09-11 19:57:00 -0700

    Created tables in chat database.

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  UserID int NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20),
+  PRIMARY KEY(UserID)
+);
+
+CREATE TABLE rooms (
+  RoomID int NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(20),
+  PRIMARY KEY(RoomID)
+); 
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT, 
+  message VARCHAR(40), 
+  UserID int,
+  RoomID int,
+  PRIMARY KEY (id),
+  FOREIGN KEY(UserID) REFERENCES users(UserID),
+  FOREIGN KEY(RoomID) REFERENCES rooms(RoomID)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
